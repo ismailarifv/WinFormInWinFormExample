57ae91095a70d8de6b5ec35504b5f44abea6bb0c
İsmail Arif Güleç <i.arifgulec8110@gmail.com>
2023-11-24 16:12:21 +0300

    Add project files.

---
+++ WinFormInWinFormExample/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace WinFormInWinFormExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.login_lbl = new System.Windows.Forms.Label();
+            this.email_txt = new System.Windows.Forms.TextBox();
+            this.password_txt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.login_btn = new System.Windows.Forms.Button();
+            this.exit_btn = new System.Windows.Forms.Button();
+            this.status_lbl = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.status_lbl);
+            this.groupBox1.Controls.Add(this.exit_btn);
+            this.groupBox1.Controls.Add(this.login_btn);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.password_txt);
+            this.groupBox1.Controls.Add(this.email_txt);
+            this.groupBox1.Controls.Add(this.login_lbl);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(48, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(296, 315);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WinFormInWinFormExample.Properties.Resources.key;
+            this.pictureBox1.Location = new System.Drawing.Point(89, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 77);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // login_lbl
+            // 
+            this.login_lbl.AutoSize = true;
+            this.login_lbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.login_lbl.ForeColor = System.Drawing.Color.DarkOrange;
+            this.login_lbl.Location = new System.Drawing.Point(112, 110);
+            this.login_lbl.Name = "login_lbl";
+            this.login_lbl.Size = new System.Drawing.Size(53, 20);
+            this.login_lbl.TabIndex = 1;
+            this.login_lbl.Text = "Login";
+            // 
+            // email_txt
+            // 
+            this.email_txt.Location = new System.Drawing.Point(68, 173);
+            this.email_txt.Name = "email_txt";
+            this.email_txt.Size = new System.Drawing.Size(173, 20);
+            this.email_txt.TabIndex = 2;
+            // 
+            // password_txt
+            // 
+            this.password_txt.Location = new System.Drawing.Point(68, 232);
+            this.password_txt.Name = "password_txt";
+            this.password_txt.PasswordChar = '*';
+            this.password_txt.Size = new System.Drawing.Size(173, 20);
+            this.password_txt.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(64, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Email";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(64, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Password";
+            // 
+            // login_btn
+            // 
+            this.login_btn.BackColor = System.Drawing.Color.ForestGreen;
+            this.login_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.login_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.login_btn.Location = new System.Drawing.Point(68, 258);
+            this.login_btn.Name = "login_btn";
+            this.login_btn.Size = new System.Drawing.Size(82, 28);
+            this.login_btn.TabIndex = 6;
+            this.login_btn.Text = "Login";
+            this.login_btn.UseVisualStyleBackColor = false;
+            this.login_btn.Click += new System.EventHandler(this.login_btn_Click);
+            // 
+            // exit_btn
+            // 
+            this.exit_btn.BackColor = System.Drawing.Color.Red;
+            this.exit_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.exit_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.exit_btn.Location = new System.Drawing.Point(156, 258);
+            this.exit_btn.Name = "exit_btn";
+            this.exit_btn.Size = new System.Drawing.Size(82, 28);
+            this.exit_btn.TabIndex = 7;
+            this.exit_btn.Text = "Exit";
+            this.exit_btn.UseVisualStyleBackColor = false;
+            this.exit_btn.Click += new System.EventHandler(this.exit_btn_Click);
+            // 
+            // status_lbl
+            // 
+            this.status_lbl.AutoSize = true;
+            this.status_lbl.Location = new System.Drawing.Point(65, 299);
+            this.status_lbl.Name = "status_lbl";
+            this.status_lbl.Size = new System.Drawing.Size(37, 13);
+            this.status_lbl.TabIndex = 8;
+            this.status_lbl.Text = "Status";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(383, 349);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button login_btn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox password_txt;
+        private System.Windows.Forms.TextBox email_txt;
+        private System.Windows.Forms.Label login_lbl;
+        private System.Windows.Forms.Button exit_btn;
+        private System.Windows.Forms.Label status_lbl;
+    }
+}
+
---
+++ WinFormInWinFormExample/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WinFormInWinFormExample
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection db = new SqlConnection(@"Data Source=DESKTOP-6I3C0B5\SQLEXPRESS;initial catalog=schoolDb;Integrated Security=True;");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            login_lbl.Visible = false;
+
+        }
+
+        private void exit_btn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void login_btn_Click(object sender, EventArgs e)
+        {
+            db.Open();
+
+            SqlCommand cmd = new SqlCommand("Login", db);
+
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add("Email", SqlDbType.NVarChar,250).Value=email_txt.Text;
+            cmd.Parameters.Add("Password", SqlDbType.NVarChar,250).Value=password_txt.Text;
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                MessageBox.Show("Giriş Başarılı");
+                Form2 form2 = new Form2();
+                form2.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("Giriş Hatalı");
+            }
+            db.Close();
+        }
+    }
+}
